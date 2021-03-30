d55b4d6c0e61f1fb6e83a2c1e0d937610d12a710
alepetpan <alepetpan@ece.auth.gr>
2021-03-30 04:45:53 +0300

    Update prod-cons.c

--- prod-cons.c
+++ prod-cons.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #define QUEUESIZE 10
-#define LOOP 20
+#define LOOP 1000000
+#define PRO_COUNT 5
+#define CON_COUNT 7
 
 void *producer (void *args);
 void *consumer (void *args);
@@ -12,12 +15,17 @@ void *consumer (void *args);
 typedef struct {
   void * (*work)(void *);
   void * arg;
+  double wait_time;
 } workFunction;
 
-void * showMe(void *i) {
-  printf("Just a message\n");
-  return &i;
+void *showMe(void *i){
+  int s = *(int *)i;
+  printf("Just Display %d\n",s);
 }
+
+struct timeval start_time, end_time;
+int counter = 0; //counter for how many items get into queue
+long double waits[LOOP*PRO_COUNT];
 
 typedef struct {
   workFunction buf[QUEUESIZE];
@@ -29,108 +37,105 @@ typedef struct {
 
 queue *queueInit (void);
 void queueDelete (queue *q);
-void queueAdd (queue *q, workFunction in);
-void queueDel (queue *q, int *out);
+void queueAdd (queue *q, workFunction *in);
+void queueDel (queue *q, workFunction *out);
+void csvfile (long double waits[], char *file);
 
 int main ()
 {
+  gettimeofday(&start_time, NULL);
   queue *fifo;
-  pthread_t pro, con;
+  pthread_t pro[PRO_COUNT], con[CON_COUNT];
+  
 
   fifo = queueInit ();
   if (fifo ==  NULL) {
     fprintf (stderr, "main: Queue Init failed.\n");
     exit (1);
   }
-  pthread_create (&pro, NULL, producer, fifo);
-  struct timeval current_time;
-  gettimeofday(&current_time, NULL);
 
-  pthread_create (&con, NULL, consumer, fifo);
-  struct timeval current_time;
-  gettimeofday(&current_time, NULL);
-  pthread_join (pro, NULL);
-  pthread_join (con, NULL);
+  for (int i=0; i<PRO_COUNT; i++)
+    pthread_create (&pro[i], NULL, producer, fifo);
+  for (int i=0; i<CON_COUNT; i++)
+    pthread_create (&con[i], NULL, consumer, fifo);
+  for (int i=0; i<PRO_COUNT; i++)
+    pthread_join (pro[i], NULL);
+  for (int i=0; i<CON_COUNT; i++)
+    pthread_join (con[i], NULL);
+
   queueDelete (fifo);
-
+  gettimeofday(&end_time, NULL);
+  long double aggreg_time = 0;
+  aggreg_time = (end_time.tv_sec - start.time.tv_sec)*1000 + (end_time.tv_usec - start_time.tv_usec)*0.001;
+  long double mean_time = 0;
+  for (int i=0; i<LOOP*PRO_COUNT; i++) {
+    mean_time += waits[i];
+  }
+  mean_time = mean_time/(LOOP*PRO_COUNT);
+  csvfile(waits, file);
+  printf("Aggregation time of program: %lf", aggreg_time);
+  printf("Mean value of waiting time: %lf", mean_time);
   return 0;
 }
 
 void *producer (void *q)
 {
-  queue *fifo;
-  workFunction producted;
+  struct timeval tstart;
 
+  queue *fifo;
   fifo = (queue *)q;
 
-  for (i = 0; i < LOOP; i++) {
+  for (int j = 0; j < LOOP; j++) {
+
+    workFunction *p1;
+    int* argum = (int*)malloc(sizeof(int));
+    p1 = (workFunction *)malloc(sizeof(workFunction));
+    p1->work = &showMe;
+    argum[0] = rand()%10;
+    p1->arg = (void *) argum;
+
     pthread_mutex_lock (fifo->mut);
     while (fifo->full) {
       printf ("producer: queue FULL.\n");
       pthread_cond_wait (fifo->notFull, fifo->mut);
     }
-    queueAdd (fifo, producted);
+    gettimeofday(&tstart, NULL);
+    p1->wait_time = tstart.tv_sec*1000 + tstart.tv_usec*0.001;
+    queueAdd (fifo, p1);
     pthread_mutex_unlock (fifo->mut);
     pthread_cond_signal (fifo->notEmpty);
-    usleep (100000);
-  }
-  for (i = 0; i < LOOP; i++) {
-    pthread_mutex_lock (fifo->mut);
-    while (fifo->full) {
-      printf ("producer: queue FULL.\n");
-      pthread_cond_wait (fifo->notFull, fifo->mut);
-    }
-    queueAdd (fifo, producted);
-    pthread_mutex_unlock (fifo->mut);
-    pthread_cond_signal (fifo->notEmpty);
-    usleep (200000);
   }
   return (NULL);
 }
 
 void *consumer (void *q)
 {
-  queue *fifo;
-  int i, d;
+  struct timeval tfinish;
+  workFunction d;
 
+  queue *fifo;
   fifo = (queue *)q;
 
-  for (i = 0; i < LOOP; i++) {
+  while (1) {
     pthread_mutex_lock (fifo->mut);
     while (fifo->empty) {
       printf ("consumer: queue EMPTY.\n");
       pthread_cond_wait (fifo->notEmpty, fifo->mut);
     }
     queueDel (fifo, &d);
+    gettimeofday(&tfinish,NULL);
+    waits[counter] = tfinish.tv_sec*1000 + tfinish.tv_usec*0.001 - d.wait_time;
+    printf("Waiting time was %lf seconds\n",waits[counter]);
+	counter++;
+    d.work(d.arg);
     pthread_mutex_unlock (fifo->mut);
     pthread_cond_signal (fifo->notFull);
-    printf ("consumer: recieved %d.\n", d);
-    usleep(200000);
-  }
-  for (i = 0; i < LOOP; i++) {
-    pthread_mutex_lock (fifo->mut);
-    while (fifo->empty) {
-      printf ("consumer: queue EMPTY.\n");
-      pthread_cond_wait (fifo->notEmpty, fifo->mut);
-    }
-    queueDel (fifo, &d);
-    pthread_mutex_unlock (fifo->mut);
-    pthread_cond_signal (fifo->notFull);
-    printf ("consumer: recieved %d.\n", d);
-    usleep (50000);
+    printf ("consumer: recieved\n");
   }
   return (NULL);
 }
 
-/*
-  typedef struct {
-  int buf[QUEUESIZE];
-  long head, tail;
-  int full, empty;
-  pthread_mutex_t *mut;
-  pthread_cond_t *notFull, *notEmpty;
-  } queue;
-*/
+
 
 queue *queueInit (void)
 {
@@ -164,9 +169,10 @@ void queueDelete (queue *q)
   free (q);
 }
 
-void queueAdd (queue *q, workFunction in)
+void queueAdd (queue *q, workFunction *in)
 {
-  q->buf[q->tail] = in;
+  gettimeofday(&start_time,NULL);
+  q->buf[q->tail] = *in;
   q->tail++;
   if (q->tail == QUEUESIZE)
     q->tail = 0;
@@ -177,7 +183,7 @@ void queueAdd (queue *q, workFunction in)
   return;
 }
 
-void queueDel (queue *q, int *out)
+void queueDel (queue *q, workFunction *out)
 {
   *out = q->buf[q->head];
 
@@ -191,15 +197,12 @@ void queueDel (queue *q, int *out)
   return;
 }
 
-/*
-    struct timeval start_time, end_time;
-    gettimeofday(&start_time, NULL);
-    long int start_sec = start_time.tv_sec;
-    long int start_usec = start_time.tv_usec;
-    sleep(2);
-    gettimeofday(&end_time, NULL);
-    long int end_sec = end_time.tv_sec;
-    long int end_usec = end_time.tv_usec;
-    printf("The program lasted %ld seconds and %ld microseconds", end_sec-start_sec, end_usec-start_usec);
-    return 0;
-*/
+void csvfile (long double waits[], char *file)
+{
+  FILE *f;
+  file = strcat(file, ".csv");
+  f = fopen(file, "w");
+  for (int i=0; i<LOOP*PRO_COUNT; i++)
+    fprintf(f, "%d , %lf", i,waits[i]);
+  fclose(f);
+}
